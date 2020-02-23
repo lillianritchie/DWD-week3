e96e0ddf6d843ec3e181a45a06341130ef833277
lillianritchie <lr2527@nyu.edu>
2020-02-23 15:41:12 -0500

    adding minimal CSS

--- rickapi.js
+++ rickapi.js
@@ -26,6 +26,8 @@ container.setAttribute('class','container');
 //add the container as a child of the root div
 app.appendChild(container);
 
+//set a random number of characters to choose
+
 //make an xml request
 var request = new XMLHttpRequest()
 //call the rick and morty API
@@ -37,6 +39,7 @@ request.onload = function() {
     var data = response["results"]
 
   if (request.status >= 200 && request.status < 400) {
+
     data.forEach(character => {
       //create a contact card with class
       const card = document.createElement('div'); 
@@ -51,13 +54,39 @@ request.onload = function() {
       //create the contact image
       const picture = document.createElement('img');
       picture.setAttribute('class','picture');
-      picture.src(this.img);
+      picture.setAttribute('src', character.image);
+
+      //create contact location
+      
+      //section to hold address
+      const address = document.createElement('section');
+      address.setAttribute('class', 'address');
+      //address content
+      const addressBold = document.createElement('p');
+      addressBold.setAttribute('class','address__bold');
+      addressBold.textContent = 'Address: ';
+      const addressPlanet = document.createElement('p');
+      addressPlanet.setAttribute('class', 'address__planet');
+      address.textContent = character.location.name;
+      //add elements to section
+      address.appendChild(addressBold);
+      address.appendChild(addressPlanet);
+     
+      
+      //create contact phone
+      const phone = document.create
+
       //print the names and contact info
       card.appendChild(contact);
+      card.appendChild(picture);
+      card.appendChild(address);
       
     })
   } else {
     console.log('error')
+    const errorMessage = document.createElement('marquee')
+    errorMessage.textContent = `Wubba Lubba Dub Dub! It's not working!`
+    app.appendChild(errorMessage)
   }
 }
 request.send()
